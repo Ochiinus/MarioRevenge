3fc9c4da476930aa429deda4c5b54fd1e47e0d0a
MathieuMesnil <mesnilmathieu1@gmail.com>
2021-10-01 16:05:11 +0200

    scene4: add script respawn

---
+++ Assets/scene4/Scripts/masseContact.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class masseContact : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnTriggerEnter(Collider col){
+        if(col.name == "First Person Controller"){
+            col.transform.position = new Vector3(39.55f, 9.59f, -71.04f);
+        }
+    }
+}
