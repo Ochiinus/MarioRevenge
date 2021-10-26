d29fb32b285047ff7a144cf309a553db217e29f4
MathieuMesnil <mesnilmathieu1@gmail.com>
2021-10-26 11:10:36 +0200

    mouvement fpm

--- Assets/Mini First Person Controller/Scripts/FirstPersonMovement.cs
+++ Assets/Mini First Person Controller/Scripts/FirstPersonMovement.cs
@@ -10,14 +10,8 @@ public class FirstPersonMovement : MonoBehaviour
     public bool IsRunning { get; private set; }
     public float runSpeed = 9;
     public KeyCode runningKey = KeyCode.LeftShift;
-    public int fowardH = 0;
-    public int fowardV = 1;
-    public int backH = 0;
-    public int backV = -1;
-    public int leftH = -1;
-    public int leftV = 0;
-    public int rightH = 0;
-    public int rightV = -1;
+    public float targetMovingSpeed;
+    public int directionStatut = 0;
 
     Rigidbody rigidbody;
     /// <summary> Functions to override movement speed. Will use the last added override. </summary>
@@ -37,62 +31,87 @@ public class FirstPersonMovement : MonoBehaviour
         IsRunning = canRun && Input.GetKey(runningKey);
 
         // Get targetMovingSpeed.
-        float targetMovingSpeed = IsRunning ? runSpeed : speed;
+        targetMovingSpeed = IsRunning ? runSpeed : speed;
         if (speedOverrides.Count > 0)
         {
             targetMovingSpeed = speedOverrides[speedOverrides.Count - 1]();
         }
 
-        // Get targetVelocity from input.
-        Vector2 targetVelocity =new Vector2( Input.GetAxis("Horizontal") * targetMovingSpeed, Input.GetAxis("Vertical") * targetMovingSpeed);
-
         if (Input.GetKey(KeyCode.Keypad8)) {
-            // Get targetVelocity from input.
-            targetVelocity =new Vector2( (Input.GetAxis("Horizontal")+fowardH) * targetMovingSpeed, (Input.GetAxis("Vertical")+fowardV) * targetMovingSpeed);
-        }
-        if (Input.GetKey(KeyCode.Keypad6)) {
-            // Get targetVelocity from input.
-            targetVelocity =new Vector2( (Input.GetAxis("Horizontal")+leftH) * targetMovingSpeed, (Input.GetAxis("Vertical")+leftV) * targetMovingSpeed);
-        }
-        if (Input.GetKey(KeyCode.Keypad5)) {
-            // Get targetVelocity from input.
-            targetVelocity =new Vector2( (Input.GetAxis("Horizontal")+rightH) * targetMovingSpeed, (Input.GetAxis("Vertical")+rightV) * targetMovingSpeed);
+            if (this.directionStatut == 0) {
+                moveForward();
+            }
+            if (this.directionStatut == 1) {
+                moveLeft();
+            }
+            if (this.directionStatut == 2) {
+                moveBackward();
+            }
         }
         if (Input.GetKey(KeyCode.Keypad4)) {
-            // Get targetVelocity from input.
-            targetVelocity =new Vector2( (Input.GetAxis("Horizontal")+backH) * targetMovingSpeed, (Input.GetAxis("Vertical")+backV) * targetMovingSpeed);
-        }
+            if (this.directionStatut == 0) {
 
-        rigidbody.velocity = transform.rotation * new Vector3(targetVelocity.x, rigidbody.velocity.y, targetVelocity.y);
+                moveLeft();
+            }
+            if (this.directionStatut == 1) {
+                moveForward();
+            }
+            if (this.directionStatut == 2) {
+                moveRight();
+            }
+        }
+        if (Input.GetKey(KeyCode.Keypad5)) {
+            if (this.directionStatut == 0) {
+
+                moveBackward();
+            }
+            if (this.directionStatut == 1) {
+                moveRight();
+            }
+            if (this.directionStatut == 2) {
+                moveForward();
+            }
+        }
+        if (Input.GetKey(KeyCode.Keypad6)) {
+            if (this.directionStatut == 0) {
+
+                moveRight();
+            }
+            if (this.directionStatut == 1) {
+                moveBackward();
+            }
+            if (this.directionStatut == 2) {
+                moveLeft();
+            }
+        }
+    }
+
+    void moveForward()
+    {
+        transform.position += Vector3.forward * this.targetMovingSpeed *Time.deltaTime;
+    }
+    void moveBackward()
+    {
+        transform.position += Vector3.back * this.targetMovingSpeed *Time.deltaTime;
+    }
+    void moveLeft()
+    {
+        transform.position += Vector3.left * this.targetMovingSpeed *Time.deltaTime;
+    }
+    void moveRight()
+    {
+        transform.position += Vector3.right * this.targetMovingSpeed *Time.deltaTime;
     }
 
     void OnTriggerExit(Collider col){
         if(col.name == "cmdDirection1") {
-            int tempForwardH = this.fowardH;
-            int tempForwardV = this.fowardV;
-            this.fowardH = this.backH;
-            this.fowardV = this.backV;
-            this.backH = this.rightH;
-            this.backV = this.rightV;
-            this.rightH = this.leftH;
-            this.rightV = this.leftV;
-            this.leftH = tempForwardH;
-            this.leftV = tempForwardV;
+            this.directionStatut = 1;
             Destroy(GameObject.Find("cmdDirection1"));
             
         }
 
         if(col.name == "cmdDirection2") {
-            int tempForwardH = this.fowardH;
-            int tempForwardV = this.fowardV;
-            this.fowardH = this.rightH;
-            this.fowardV = this.rightV;
-            this.rightH = this.leftH;
-            this.rightV = this.leftV;
-            this.leftH = this.backH;
-            this.leftV = this.backV;
-            this.backH = tempForwardH;
-            this.backV = tempForwardV;
+            this.directionStatut = 2;
             Destroy(GameObject.Find("cmdDirection2"));
         }
     }
